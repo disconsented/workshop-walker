4498011a46f2f6058949f8de07b2ad6802754ca4
James Kerr <disconsented@disconsented.com>
2025-04-24 14:16:00 +1200

    feat: Expand language support
    I'd noticed Japanese and Korean mods, so, adding support for those.

--- src/language.rs
+++ src/language.rs
@@ -1,34 +1,47 @@
-use std::convert::Into;
+use std::{convert::Into, fmt};
 
 use lingua::{
     Language,
-    Language::{Chinese, English, Russian},
+    Language::{Chinese, English, Japanese, Korean, Russian},
     LanguageDetectorBuilder,
 };
 use salvo::prelude::ToSchema;
 use serde::{Deserialize, Serialize};
+use crate::language::DetectedLanguage::Unknown;
 
-#[derive(Debug, Default, ToSchema, Copy, Clone, Serialize, Deserialize,)]
+#[derive(Debug, Default, ToSchema, Copy, Clone, Serialize, Deserialize)]
 pub enum DetectedLanguage {
     #[default]
     English,
     Russian,
     Chinese,
+    Japanese,
+    Korean,
+    Unknown
 }
 
+impl fmt::Display for DetectedLanguage {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{:?}", self)
+    }
+}
 impl From<Language> for DetectedLanguage {
     fn from(value: Language) -> Self {
         match value {
             Chinese => Self::Chinese,
             Russian => Self::Russian,
-            _ => Self::English,
+            Japanese => Self::Japanese,
+            Korean => Self::Korean,
+            English => Self::English,
+            _ => Unknown
         }
     }
 }
 pub fn detect(text: &str) -> DetectedLanguage {
-    let detector = LanguageDetectorBuilder::from_languages(&[English, Russian, Chinese])
-        .with_minimum_relative_distance(0.9)
-        .build();
+    let detector =
+        LanguageDetectorBuilder::from_languages(&[English, Russian, Chinese, Japanese, Korean])
+            .with_minimum_relative_distance(0.9)
+            .build();
     detector
         .detect_language_of(text)
         .map(Into::into)
