f0d3647c29a80b23238b86159e787543be45a0d0
James Kerr <disconsented@disconsented.com>
2025-04-29 11:50:18 +1200

    feat: Add timeAgo.svelte component

---
+++ ui/src/lib/timeAgo.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import TimeAgo from 'javascript-time-ago';
+	import en from 'javascript-time-ago/locale/en';
+	TimeAgo.addDefaultLocale(en);
+	let { date }: { date: number } = $props();
+	const timeAgo = new TimeAgo('en-US');
+</script>
+
+{timeAgo.format(new Date(date * 1000))}
