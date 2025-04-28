20e31eae11617ebc37e7c9eac787fc96f7fb21ab
James Kerr <disconsented@disconsented.com>
2025-04-29 11:50:05 +1200

    feat: Add support for language, order_by and limit to the backend requests

--- ui/src/routes/app/[id]/+page.ts
+++ ui/src/routes/app/[id]/+page.ts
@@ -1,16 +1,22 @@
-import { language, tags } from './store.svelte';
+import { orderBy, language, tags, limit } from './store.svelte';
 import type { PageLoad } from '../../../../.svelte-kit/types/src/routes/app/[id]/$types';
 export const prerender = false;
 export const load: PageLoad = async ({ fetch, params }) => {
-	// const url = new URL();
-
 	let paramList = [];
 	if (language.v) {
+		paramList.push(['language', language.v]);
 	}
 	if (tags.v) {
 		tags.v.forEach((tag) => {
 			paramList.push(['tags', tag]);
 		});
+	}
+	if (orderBy.v) {
+		paramList.push(['order_by', orderBy.v]);
+	}
+
+	if (limit.v) {
+		paramList.push(['limit', limit.v]);
 	}
 
 	const searchParams = new URLSearchParams(paramList);
--- ui/src/routes/app/[id]/store.svelte.ts
+++ ui/src/routes/app/[id]/store.svelte.ts
@@ -1,3 +1,4 @@
-export const language = $state({v: 'English'});
+export const language = $state({ v: 'English' });
 export const tags = $state({ v: ['Mod', '1.5'] });
-export const orderBy = $state({ v: 'alphabetical' });
+export const orderBy = $state({ v: 'LastUpdated' });
+export const limit = $state({ v: 25 });
