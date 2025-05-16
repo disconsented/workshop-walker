50fbd5a37ea378eb0db311331d67ec1670877fe1
James Kerr <disconsented@disconsented.com>
2025-05-16 15:54:24 +1200

    feat: Add title/lastUpdated to stores

--- ui/src/routes/app/[id]/store.svelte.ts
+++ ui/src/routes/app/[id]/store.svelte.ts
@@ -2,3 +2,5 @@ export const language = $state({ v: '1' });
 export const tags = $state({ v: ['Mod', '1.5'] });
 export const orderBy = $state({ v: 'LastUpdated' });
 export const limit = $state({ v: 25 });
+export const title = $state({ v: undefined });
+export const lastUpdated = $state({ v: undefined });
