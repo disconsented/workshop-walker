eaffdf2a6ed65762656024e269dd9055c279e63e
James Kerr <disconsented@disconsented.com>
2025-06-09 14:39:14 +1200

    chore: Update default limit for app search to 50

--- ui/src/routes/app/[id]/store.svelte.ts
+++ ui/src/routes/app/[id]/store.svelte.ts
@@ -1,6 +1,6 @@
 export const language = $state({ v: '1' });
 export const tags = $state({ v: ['Mod', '1.5'] });
 export const orderBy = $state({ v: 'LastUpdated' });
-export const limit = $state({ v: 25 });
+export const limit = $state({ v: 50 });
 export const title = $state({ v: undefined });
 export const lastUpdated: {v: Date | undefined} = $state({ v: undefined });
