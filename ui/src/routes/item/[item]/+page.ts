2ab844b48065324c595ccdcdd933bba61ddfc7b8
James Kerr <disconsented@disconsented.com>
2025-12-22 15:58:20 +1300

    refactor: simplify load function in item page

--- ui/src/routes/item/[item]/+page.ts
+++ ui/src/routes/item/[item]/+page.ts
@@ -1,7 +1,8 @@
 import type { PageLoad } from '../../../../.svelte-kit/types/src/routes';
+
 export const prerender = false;
 export const load: PageLoad = async ({ fetch, params }) => {
 	const res = await fetch(`/api/item/${params.item}`);
-	const item = await res.json();
-	return item;
+
+	return await res.json();
 };
