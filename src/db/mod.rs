6e242a8192ca437b7c8218b04ad39978f52a2849
James Kerr <disconsented@disconsented.com>
2026-01-26 13:49:49 +1300

    test: add SurrealDB transaction/throw tests

--- src/db/mod.rs
+++ src/db/mod.rs
@@ -13,3 +13,39 @@ use macros::define_id;
 
 define_id!("users", UserID, String);
 define_id!("workshop_items", ItemID, String);
+
+#[cfg(test)]
+mod test {
+    use surrealdb::{Surreal, engine::local::Mem};
+
+    #[tokio::test]
+    async fn test_throw() {
+        let db = Surreal::new::<Mem>(()).await.unwrap();
+        db.use_ns("test").use_db("test").await.unwrap();
+        let output = db
+            .query("DEFINE TABLE OVERWRITE properties TYPE NORMAL SCHEMAFULL PERMISSIONS NONE;")
+            .query("CREATE properties:ShouldExist;")
+            .query("BEGIN TRANSACTION;")
+            .query("CREATE properties:NotExist;")
+            .query("IF true{THROW \"GRACEFUL\"};")
+            .query("SELECT * FROM properties;")
+            .await
+            .unwrap();
+        dbg!(output.check().unwrap());
+    }
+
+    #[tokio::test]
+    async fn test_throw_commit() {
+        let db = Surreal::new::<Mem>(()).await.unwrap();
+        db.use_ns("test").use_db("test").await.unwrap();
+        let query = r#"BEGIN;
+            LET $person = CREATE ONLY person SET age = rand::int(1,5);
+            IF $person.age == 5 {
+                THROW "Whoops";
+            };
+            CREATE person;
+            COMMIT;"#;
+        let output = db.query(query).await.unwrap();
+        dbg!(output.check().unwrap());
+    }
+}
