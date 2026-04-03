ecdd85d58bc7e6aebb0a2aeb309cd6cc10bbb638
James Kerr <disconsented@disconsented.com>
2026-04-03 13:17:58 +1300

    stash

--- scratch/src/lib.rs
+++ scratch/src/lib.rs
@@ -2,62 +2,19 @@ use macros::define_id;
 
 define_id!("users", Internal, External, String);
 
-// ///   Externally facing newtype over $external_type
-// #[derive(serde::Serialize, serde::Deserialize, Clone, Debug)]
-// #[serde(transparent)]
-// pub struct External(String);
-// impl Into<String> for External {
-//     fn into(self) -> String {
-//         self.0
-//     }
-// }
-// impl From<String> for External {
-//     fn from(id: String) -> Self {
-//         Self(id)
-//     }
-// }
-// ///   Internally facing newtype over RecordID
-// #[derive(serde::Serialize, serde::Deserialize, Clone, Debug)]
-// #[serde(transparent)]
-// pub struct Internal(surrealdb_types::RecordId);
-// impl Internal {
-//     const TABLE_NAME: &'static str = "users";
-// }
-// impl From<External> for Internal {
-//     fn from(id: External) -> Self {
-//         Self(surrealdb_types::RecordId::new(Self::TABLE_NAME, id.0))
-//     }
-// }
-// impl Into<surrealdb_types::RecordId> for Internal {
-//     fn into(self) -> surrealdb_types::RecordId {
-//         self.0
-//     }
-// }
-// impl TryInto<External> for Internal {
-//     type Error = surrealdb_types::Error;
-//
-//     fn try_into(self) -> Result<External, Self::Error> {
-//         Ok(External(
-//             surrealdb_types::SurrealValue::into_value(self.0.key).into_t()?,
-//         ))
-//     }
-// }
-
 #[cfg(test)]
 mod tests {
-    use derive_more::Into;
-
-    // use macros::define_id;
+    use proc_macros::ConvertId;
     use crate::{External, Internal};
 
-    #[derive(Into)]
+    #[derive(ConvertId)]
     struct TestStruct<T> {
         id: T,
         something: String,
     }
 
     #[test]
-    fn it_works() {
+    fn test_type_conversion() {
         let external = External::from("test".to_string());
         let internal = Internal::from(external);
         let external2: External = internal.try_into().unwrap();
@@ -67,12 +24,13 @@ mod tests {
     }
 
     #[test]
-    fn it_works_2() {
+    fn test_struct_conversion() {
         let external = TestStruct::<External> {
             id: String::from("test").into(),
             something: "".to_string(),
         };
 
         let internal: TestStruct<Internal> = external.into();
+        let _external2: TestStruct<External> = internal.try_into().unwrap();
     }
 }
