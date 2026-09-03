ab5949abc56274a55bc58f6154bce0b8bb829ea1
James Kerr <disconsented@disconsented.com>
2026-09-03 13:28:52 +1200

    build: enable the mold linker again

--- classification/src/lib.rs
+++ classification/src/lib.rs
@@ -86,11 +86,8 @@ pub enum Error {
 #[derive(Debug, Snafu)]
 #[snafu(whatever)]
 #[snafu(display("{message}"))]
-// #[snafu(provide(opt, ref, chain, dyn std::error::Error + Send + Sync =>
-// source.as_deref()))]
 pub struct WhateverAsync {
     #[snafu(source(from(Box<dyn std::error::Error + Send + Sync>, Some)))]
-    // #[snafu(provide(false))]
     source: Option<Box<dyn std::error::Error + Send + Sync>>,
     message: String,
     backtrace: Backtrace,
